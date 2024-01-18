afdaf761d80d8d343e69076cb8896090ed2ed919
John-Wensink <john.wensink@csuglobal.edu>
2024-01-18 17:15:56 -0500

    Add files via upload

---
+++ Week1Part1.py
@@ -0,0 +1,7 @@
+#Week 1 - Part 1
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+difference = num1 - num2
+print("The Sum is", sum)
+print("The Difference is", difference)
