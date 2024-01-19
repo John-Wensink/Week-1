6871e4ec3783c48630bb852cc962dab9f9163350
John-Wensink <john.wensink@csuglobal.edu>
2024-01-19 13:58:33 -0500

    Update AgeBot1.py

--- AgeBot1.py
+++ AgeBot1.py
@@ -1,17 +1,17 @@
-age=int(input("What is your age?"))
-if age<18:
+age = int(input("What is your age?"))
+if age < 18:
   print("You are a child grow up")
 else:
   print("You are an adult go to work")
-brains=bool(input("Are you smart?"))
-if brains==True:
-  print("Go to school")
-else:
+brains = bool(input("Are you smart?"))
+if not brains:
   print("Go to work")
-lazy=int(input("How lazy are you from 1-10?"))
-if lazy<3:
-  print("You are going to be homeless")
-if lazy <= 7:  # Corrected comparison
+else:
+  print("Go to school")
+lazy = int(input("How lazy are you from 1-10?"))
+if lazy <= 4:
+  print("You are going to be a millionaire")
+elif lazy <= 7:
   print("You are going to be a bum")
 else:
-  print("You are going to be a millionaire")
+  print("You are going to be homeless")
